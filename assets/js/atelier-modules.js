27eb17c4650b8334b835ecbee088478745a15785
Vinicius Martins <iheyfoxes0fficial@gmail.com>
2026-09-23 12:58:13 -0300

    refactor: desativar loader legado do atelier

--- assets/js/atelier-modules.js
+++ assets/js/atelier-modules.js
@@ -1,15 +1,4 @@
-/* Compatibilidade: os módulos de Clientes e Materiais foram separados. */
-(function(){
-  const base='assets/js/features/';
-  const page=location.pathname.split('/').pop();
-  const modules={
-    'clientes.html':'clientes/clientes.js',
-    'materiais.html':'materiais/materiais.js'
-  };
-  const file=modules[page];
-  if(!file)return;
-  const s=document.createElement('script');
-  s.src=base+file;
-  s.async=false;
-  document.head.appendChild(s);
-})();
+/* Compatibilidade legada.
+ * Clientes e Materiais agora são carregados pelo assets/js/app.js.
+ * Mantido temporariamente porque algumas páginas ainda referenciam este arquivo.
+ */
