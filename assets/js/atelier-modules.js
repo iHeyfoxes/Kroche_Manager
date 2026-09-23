444f3be8a40098cf96f4739ae9d84a0ea2034b1d
Vinicius Martins <iheyfoxes0fficial@gmail.com>
2026-09-23 13:13:45 -0300

    refactor: remover atelier modules legado

--- assets/js/atelier-modules.js
+++
@@ -1,4 +0,0 @@
-/* Compatibilidade legada.
- * Clientes e Materiais agora são carregados pelo assets/js/app.js.
- * Mantido temporariamente porque algumas páginas ainda referenciam este arquivo.
- */
